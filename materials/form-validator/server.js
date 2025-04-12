5369d18fe711e14635697d1b68dedbb7ea777d3f
Andre <andre.aleman.contact@gmail.com>
2025-04-12 03:46:43 -0600

    finished 1/2 of form-validator - TOP

---
+++ materials/form-validator/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('node:path');
+const app = express();
+const usersRouter = require('./src/routes/usersRouter');
+
+app.use(express.static(path.join(__dirname, 'src', 'views')));
+
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', usersRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
